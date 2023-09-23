d48128b8aa5bc63c0fdf13f72bb552c2f754f2e1
arrowwhi <arrowwhi@student.21-school.ru>
2023-09-23 17:33:56 +0300

    Init commit

---
+++ src/config.py
@@ -0,0 +1,13 @@
+TOKEN = '5782851793:AAG5tDbvIrqOlolMrLDN53mMXHvmnJ2xsiY'
+
+admin_id = 918616493
+
+group_id = 0
+
+openai_api_key = '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'
+
+webapp_url = 'https://google.com'
+
+
+def catalog_id():
+    return None
